f94858a3bf898f1f177a7590a922a62776cf2185
Ubuntu <ubuntu@ip-172-31-24-123.us-west-2.compute.internal>
2016-05-27 02:27:06 +0000

    iter 100

--- test_lda.py
+++ test_lda.py
@@ -9,7 +9,7 @@ titles = lda.datasets.load_reuters_titles()
 X.shape
 X.sum()
 #model = lda.LDA(n_topics=20, n_iter=1500, random_state=1)
-model = GSDMM(n_topics=20, n_iter=30, random_state=1)
+model = GSDMM(n_topics=20, n_iter=100, random_state=910820)
 model.fit(X)  # model.fit_transform(X) is also available
 topic_word = model.topic_word_  # model.components_ also works
 n_top_words = 8
